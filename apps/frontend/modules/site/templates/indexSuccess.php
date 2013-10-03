9de58c0910cda2b794ebdb05fd3fb7eec7527b80
Leonardo Podda <leonardo.podda@gmail.com>
2013-10-03 18:11:28 +0200

    frontend app

--- apps/frontend/modules/site/templates/indexSuccess.php
+++ apps/frontend/modules/site/templates/indexSuccess.php
@@ -1,44 +1,13 @@
-<div class="jumbotron">
-    <h2>Lightbox image gallery</h2>
-    <!-- The container for the list of example images -->
-    <div id="links" class="links"></div>
-
-    <!-- The Gallery as lightbox dialog, should be a child element of the document body -->
-    <div id="blueimp-gallery" class="blueimp-gallery">
-        <div class="slides"></div>
-        <h3 class="title"></h3>
-        <a class="prev">‹</a>
-        <a class="next">›</a>
-        <a class="close">×</a>
-        <a class="play-pause"></a>
-        <ol class="indicator"></ol>
-    </div>
-</div>
-
-<hr>
-
+<script>
+    $('#datepicker-example').Zebra_DatePicker({
+        direction: 1    // boolean true would've made the date picker future only
+        // but starting from today, rather than tomorrow
+    });
+</script>
 <!-- Example row of columns -->
 <div class="row-fluid">
-    <div class="span6">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn" href="#">View details &raquo;</a></p>
-    </div>
-    <div class="span6">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn" href="#">View details &raquo;</a></p>
-</div>
-<div class="row-fluid">
-    <div class="span6">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-        <p><a class="btn" href="#">View details &raquo;</a></p>
-    </div>
-    <div class="span6">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-        <p><a class="btn" href="#">View details &raquo;</a></p>
+    <div class="span12">
+        <div id="datepicker-example"></div>
     </div>
 
 </div>
